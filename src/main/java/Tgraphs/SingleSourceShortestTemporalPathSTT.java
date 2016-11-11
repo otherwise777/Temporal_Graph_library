5dad0ed024cede485aef6a539fa4dc8ebb08a3d8
otherwise777 <commits@onzichtbaar.net>
2016-11-11 15:31:41 +0100

    its running but not working

--- src/main/java/Tgraphs/SingleSourceShortestTemporalPathSTT.java
+++ src/main/java/Tgraphs/SingleSourceShortestTemporalPathSTT.java
@@ -3,6 +3,7 @@ package Tgraphs;
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.graph.Edge;
 import org.apache.flink.graph.Vertex;
@@ -12,6 +13,7 @@ import org.apache.flink.graph.spargel.ScatterFunction;
 import org.apache.flink.types.NullValue;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by s133781 on 03-Nov-16.
@@ -26,7 +28,8 @@ public class SingleSourceShortestTemporalPathSTT<K,EV> implements TGraphAlgorith
 
     private final K srcVertexId;
     private final Integer maxIterations;
-    private DataSet<Double> startedges;
+    private DataSet<Tuple2<Double,Double>> startedges;
+    private ArrayList<Tuple2<Double,Double>> listedges;
 
     public SingleSourceShortestTemporalPathSTT(K srcVertexId, Integer maxIterations) {
         this.srcVertexId = srcVertexId;
@@ -36,21 +39,28 @@ public class SingleSourceShortestTemporalPathSTT<K,EV> implements TGraphAlgorith
     public DataSet<Vertex<K,Double>> run(Tgraph<K, NullValue, EV, Double> input) throws Exception {
         //Filling up the startingedges with distinct values of the starting times of the starting edges
         DataSet<Edge<K,Tuple3<EV,Double,Double>>> startedgesset = input.getGellyGraph().getEdges().filter(new Filterstartingedges(srcVertexId));
-        startedges = startedgesset.map(new MapFunction<Edge<K, Tuple3<EV, Double, Double>>, Double>() {
+        startedges = startedgesset.map(new MapFunction<Edge<K, Tuple3<EV, Double, Double>>, Tuple2<Double,Double>>() {
             @Override
-            public Double map(Edge<K, Tuple3<EV, Double, Double>> value) throws Exception {
-                return value.getValue().f1;
+            public Tuple2<Double,Double> map(Edge<K, Tuple3<EV, Double, Double>> value) throws Exception {
+                return new Tuple2<>(value.getValue().f1,Double.MAX_VALUE);
             }
         }).distinct();
 
-
-
-        startedges.print();
-        return input.getGellyGraph().mapVertices(new InitVerticesMapper<K>(srcVertexId)).runScatterGatherIteration(
+        listedges = (ArrayList<Tuple2<Double, Double>>) startedges.collect();
+//        System.out.println("printing edges");
+//        System.out.println(listedges.toString());
+//        startedges.print();
+        input.getGellyGraph().mapVertices(new InitVerticesMapper<K>(srcVertexId,listedges)).runScatterGatherIteration(
                 new MinDistanceMessengerforTuplewithpath<K,EV>(), new VertexDistanceUpdaterwithpath<K>(),
-                maxIterations).getVertices();
+                maxIterations).getVertices().print();
+        return input.getGellyGraph().mapVertices(new InitVerticesMapper<K>(srcVertexId,listedges)).runScatterGatherIteration(
+                new MinDistanceMessengerforTuplewithpath<K,EV>(), new VertexDistanceUpdaterwithpath<K>(),
+                maxIterations).mapVertices(new finalVerticesMapper<>()).getVertices();
 
     }
+    /*
+    * Filter to filter out the edges which have srcvertexid as source veretx
+    * */
     public class Filterstartingedges implements FilterFunction<Edge<K,Tuple3<EV,Double,Double>>> {
         private K srcVertexId;
         public Filterstartingedges(K srcVertexId) { this.srcVertexId = srcVertexId; }
@@ -65,24 +75,46 @@ public class SingleSourceShortestTemporalPathSTT<K,EV> implements TGraphAlgorith
         }
     }
 
-    public static final class InitVerticesMapper<K>	implements MapFunction<Vertex<K, NullValue>, Double> {
+    public static final class InitVerticesMapper<K>	implements MapFunction<Vertex<K, NullValue>, ArrayList<Tuple2<Double,Double>>> {
 
         private K srcVertexId;
+        private ArrayList<Tuple2<Double,Double>> startedges;
 
-        public InitVerticesMapper(K srcId) {
+        public InitVerticesMapper(K srcId, ArrayList<Tuple2<Double,Double>> startedges) {
+            this.startedges = startedges;
             this.srcVertexId = srcId;
         }
 
-        public Double map(Vertex<K, NullValue> value) throws Exception {
+        /*
+        * adds the startingedge dataset to every edge, and changes the starting vertex with null values in that dataset
+        * */
+        public ArrayList<Tuple2<Double,Double>> map(Vertex<K, NullValue> value) throws Exception {
+            ArrayList<Tuple2<Double,Double>> nodeList = new ArrayList<>();
              if (value.f0.equals(srcVertexId)) {
-                return 0.0;
+                 for (Tuple2<Double,Double> tuple: startedges) {
+                     nodeList.add(new Tuple2<>(tuple.f0,0D));
+                 }
+                return nodeList;
             } else {
-                return Double.MAX_VALUE;
+                return new ArrayList<>(startedges);
             }
         }
     }
-
-
+    /*
+    * final mapper, gets arraylist with values and maps it to
+    * */
+    public static final class finalVerticesMapper<K>	implements MapFunction<Vertex<K, ArrayList<Tuple2<Double,Double>>>, Double> {
+        @Override
+        public Double map(Vertex<K, ArrayList<Tuple2<Double, Double>>> value) throws Exception {
+            Double mindist = Double.MAX_VALUE;
+            for (Tuple2<Double,Double> tuple : value.getValue()) {
+                if(tuple.f1 - tuple.f0 < mindist) {
+                    mindist = tuple.f1 - tuple.f0;
+                }
+            }
+            return mindist;
+        }
+    }
     /*
     * mindistance function from scatterfunction with:
     * K as K
@@ -93,18 +125,21 @@ public class SingleSourceShortestTemporalPathSTT<K,EV> implements TGraphAlgorith
     * checks if vertexvalue is < inf, then sends a message to neighboor vertexes
     * with end time and such
     * */
-    private static final class MinDistanceMessengerforTuplewithpath<K,EV> extends ScatterFunction<K, Double, Double, Tuple3<EV, Double, Double>> {
+    private static final class MinDistanceMessengerforTuplewithpath<K,EV> extends ScatterFunction<K, ArrayList<Tuple2<Double,Double>>, Tuple2<Double,Double>, Tuple3<EV, Double, Double>> {
         @Override
-        public void sendMessages(Vertex<K, Double> vertex) {
-            if (vertex.getValue() < Double.POSITIVE_INFINITY) { //Checks if it has been passed for the first time
-                for (Edge<K, Tuple3<EV,Double,Double>> edge : getEdges()) {
-                    if (edge.getValue().f1 >= vertex.getValue()) { //If starting time of the edge
-                        sendMessageTo(edge.getTarget(), edge.getValue().f2.doubleValue());
+        public void sendMessages(Vertex<K, ArrayList<Tuple2<Double, Double>>> vertex) throws Exception {
+            for (Edge<K, Tuple3<EV, Double, Double>> edge : getEdges()) {
+                for (Tuple2<Double,Double> tuple : vertex.getValue()) {
+//                    check if the starting time of the edge is geq final time of the vertex
+                    if(edge.getValue().f1 >= tuple.f1) {
+//                        sends message to the target vertex with starting time of the node and finishing tiem of the edge
+                        sendMessageTo(edge.getTarget(),new Tuple2<>(tuple.f0,edge.getValue().f2));
                     }
                 }
             }
         }
     }
+
     /**
      * K as K
      * VV as Tuple2<Double,ArrayList<K>>
@@ -112,27 +147,37 @@ public class SingleSourceShortestTemporalPathSTT<K,EV> implements TGraphAlgorith
      *
      * @param <K>
      */
+    private static final class VertexDistanceUpdaterwithpath<K> extends GatherFunction<K, ArrayList<Tuple2<Double, Double>>, Tuple2<Double,Double>> {
 
+//        @Override
+//        public void updateVertex(Vertex<K, Double> vertex, MessageIterator<Double> inMessages) {
+//
+//            Double minDistance = Double.MAX_VALUE;
+//            for (Double msg : inMessages) {
+//                if (msg < minDistance) {
+//                    minDistance = msg;
+//                }
+//            }
+//
+//            if (vertex.getValue() > minDistance) {
+//                setNewVertexValue(minDistance);
+//            }
+//        }
 
-    private static final class VertexDistanceUpdaterwithpath<K> extends GatherFunction<K, Double, Double> {
 
         @Override
-        public void updateVertex(Vertex<K, Double> vertex, MessageIterator<Double> inMessages) {
+        public void updateVertex(Vertex<K, ArrayList<Tuple2<Double, Double>>> vertex, MessageIterator<Tuple2<Double,Double>> inMessages) throws Exception {
+            ArrayList<Tuple2<Double,Double>> minimums = new ArrayList<>(vertex.getValue());
 
-            Double minDistance = Double.MAX_VALUE;
-            for (Double msg : inMessages) {
-                if (msg < minDistance) {
-                    minDistance = msg;
+            for (Tuple2<Double,Double> tuple: inMessages)  {
+                for(Tuple2<Double,Double> min : minimums) {
+                    if(tuple.f0.equals(min.f0) && tuple.f1 < min.f1) {
+//                        starting times are the same but new path is smaller
+                        min.f1 = tuple.f1;
+                    }
                 }
             }
-
-            if (vertex.getValue() > minDistance) {
-                setNewVertexValue(minDistance);
-            }
+            setNewVertexValue(minimums);
         }
-
-
     }
-
-
 }
